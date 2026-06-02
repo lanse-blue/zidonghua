3cf6b6c44f50093e1bf98ae9943b474d69188335
lanse-blue <1079478339@qq.com>
2026-06-02 14:46:21 +0800

    第二次提交

---
+++ src/common/getpath.py
@@ -0,0 +1,25 @@
+import os
+class GetPath:
+    def get_pro_path(self):
+        cur_filepath = os.path.realpath(__file__)
+        pro_path = os.path.dirname(os.path.dirname(os.path.dirname(cur_filepath)))
+        return pro_path
+    def get_data_path(self):
+        data_path = os.path.join(self.get_pro_path(), 'data')
+        return data_path
+    def get_config_path(self):
+        config_path = os.path.join(self.get_pro_path(), 'config')
+        return config_path
+    def get_picture_path(self):
+        picture_path = os.path.join(self.get_pro_path(), 'picture')
+        return picture_path
+    def get_reports_path(self):
+        reports_path = os.path.join(self.get_pro_path(), 'reports')
+        return reports_path
+    def get_testcase_path(self):
+        testcase_path = os.path.join(self.get_pro_path(), 'testcase')
+        return testcase_path
+    def get_pages_path(self):
+        pages_path = os.path.join(self.get_pro_path(), 'pages')
+        return pages_path
+get_path = GetPath()
--- src/common/read_csv.py
+++ src/common/read_csv.py
@@ -1,25 +1,39 @@
 import os
 import csv
+from getpath import *
 
 
 # 定义一个类，用来读取csv文件的数据（测试用例数据和配置数据）
 class ReadCSV:
-    def read_case_data(self, file_name):
+
+    def read_case_data(self, file_name, row_num):
         """
         :param file_name: 文件名
-        :return:
+        :param row_num: 行数
+        :return:返回的是需要测试的用例的数据，数据类型为列表
         """
-        cur_filepath = os.path.abspath(__file__)
-        pro_path = os.path.dirname(os.path.dirname(os.path.dirname(cur_filepath)))
-        csv_path = os.path.join(pro_path, 'data', file_name)
-        print(csv_path)
+        csv_path = os.path.join(get_path.get_data_path(), file_name)
 
         with open(csv_path, 'r', encoding='utf-8') as f:
             csv_data = csv.reader(f)
-            print(csv_data,type(csv_data))
+            print(csv_data, type(csv_data))
             data = list(csv_data)
+        return data[row_num - 1]
+
+    def read_config_data(self, file_name):
+        """
+        :param file_name: 文件名
+        :param row_num: 行数
+        :return:返回的是需要测试的用例的数据，数据类型为列表
+        """
+        csv_path = os.path.join(get_path.get_config_path(), file_name)
+        with open(csv_path, 'r', encoding='utf-8') as f:
+            csv_data = csv.reader(f)
+            print(csv_data, type(csv_data))
+            data = dict(csv_data)
         return data
 
 
 if __name__ == '__main__':
-    print(ReadCSV().read_case_data('login_case_data.csv'))
+    print(ReadCSV().read_case_data('login_case_data.csv', 1))
+    print(ReadCSV().read_config_data('url.csv'))
